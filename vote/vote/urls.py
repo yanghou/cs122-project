5dcdfae24e45e0ac38cc84908a8a65fa6d9ca560
Anne Rogers <amr@cs.uchicago.edu>
2017-03-09 00:02:18 -0600

    add vote

---
+++ vote/vote/urls.py
@@ -0,0 +1,23 @@
+"""vote URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url,include
+from django.contrib import admin
+from vote_test import views1
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^$',views1.index,name = 'index'),
+#    url(r'^vote_test/',include('vote_test.urls'))
+]
